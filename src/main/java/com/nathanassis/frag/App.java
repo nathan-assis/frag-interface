440e56397ccea7cc267eabc731579fa55d769e13
nathanassis <75035881+nathanassis@users.noreply.github.com>
2025-05-18 10:57:02 -0300

    feat(main): render screen 1

--- src/main/java/com/nathanassis/frag/App.java
+++ src/main/java/com/nathanassis/frag/App.java
@@ -1,6 +1,9 @@
 package com.nathanassis.frag;
 
+import com.nathanassis.frag.controllers.MainController;
+import com.nathanassis.frag.utils.FXMLLoaderUtil;
 import java.io.IOException;
+import java.net.URL;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,24 +11,34 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
+  private static FXMLLoader loader = new FXMLLoader();
+  private static Scene scene = null;
+  private static String file = "Main";
 
-  private static Scene scene;
+  private static void initLoader() {
+    URL location = FXMLLoaderUtil.getLocation(file);
+    loader.setLocation(location);
+  }
+
+  private void setMainContent() throws IOException {
+    MainController controller = loader.getController();
+    if (controller != null) {
+      controller.setContent("Logo");
+    } else {
+      throw new IOException("Controller not found.");
+    }
+  }
 
   @Override
   public void start(Stage stage) throws IOException {
-    String path = "/com/nathanassis/frag/fxml/primary.fxml";
-    scene = new Scene(loadFXML(path), 640, 480);
+    initLoader();
+
+    Parent main = loader.load();
+    setMainContent();
+
+    scene = new Scene(main, 640, 480);
     stage.setScene(scene);
     stage.show();
-  }
-
-  public static void setRoot(String fxml) throws IOException {
-    scene.setRoot(loadFXML(fxml));
-  }
-
-  private static Parent loadFXML(String fxml) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml));
-    return fxmlLoader.load();
   }
 
   public static void main(String[] args) {
