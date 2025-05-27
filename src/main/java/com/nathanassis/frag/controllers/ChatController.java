c0d2905d031b90ea1c74ced9e679c9abc39a9382
nathanassis <75035881+nathanassis@users.noreply.github.com>
2025-05-27 13:28:47 -0300

    refactor: update controllers

--- src/main/java/com/nathanassis/frag/controllers/ChatController.java
+++ src/main/java/com/nathanassis/frag/controllers/ChatController.java
@@ -1,15 +1,28 @@
 package com.nathanassis.frag.controllers;
 
+import com.nathanassis.frag.core.Controller;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
-public class ChatController {
-    @FXML private VBox messageList;
+public class ChatController implements Controller {
+  @FXML private VBox messages;
+  private Parent root;
 
-    public void addMessage(String message) {
-        Label label = new Label(message);
+  @Override
+  public Parent getRoot() {
+    return root;
+  }
 
-        messageList.getChildren().add(label);
-    }
+  @Override
+  public void setRoot(Parent root) {
+    this.root = root;
+  }
+
+  public void addMessage(String message) {
+    Label label = new Label(message);
+
+    this.messages.getChildren().add(label);
+  }
 }
