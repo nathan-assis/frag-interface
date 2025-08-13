61a12c5ffe6f3c1a93ab624db470387f5a8da3ea
nathanassis <75035881+nathanassis@users.noreply.github.com>
2025-08-13 16:09:07 -0300

    feat: add jackson dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,14 @@ module com.nathanassis.frag {
   requires javafx.controls;
   requires javafx.fxml;
   requires java.net.http;
+  requires com.fasterxml.jackson.databind;
 
   opens com.nathanassis.frag to
       javafx.fxml;
   opens com.nathanassis.frag.controllers to
       javafx.fxml;
+  opens com.nathanassis.frag.dto to com.fasterxml.jackson.databind;
+  opens com.nathanassis.frag.services to com.fasterxml.jackson.databind;
 
   exports com.nathanassis.frag;
 }
